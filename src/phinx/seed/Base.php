77adcbe4661ecd5c20db14c9e6ca10f8b304a141
IT小强xqitw.cn <mail@xqitw.cn>
2019-11-02 14:51:52 +0800

    seed 命令行基类

---
+++ src/phinx/seed/Base.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  ==================================================================
+ *        文 件 名: Base.php
+ *        概    要: seed 命令行基类
+ *        作    者: IT小强
+ *        创建时间: 2019-11-02 14:16
+ *        修改时间:
+ *        copyright (c) 2016 - 2019 mail@xqitw.cn
+ *  ==================================================================
+ */
+
+namespace itxq\phinx\seed;
+
+use itxq\phinx\Phinx;
+
+/**
+ * seed 命令行基类
+ * Class Base
+ * @package itxq\phinx\seed
+ */
+abstract class Base extends Phinx
+{
+
+}
