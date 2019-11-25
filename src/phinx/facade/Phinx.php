17944c344e25d58a2e003d09bbfbcfe01234e288
IT小强xqitw.cn <mail@xqitw.cm>
2019-11-25 09:37:07 +0800

    注释优化

--- src/phinx/facade/Phinx.php
+++ src/phinx/facade/Phinx.php
@@ -12,12 +12,21 @@
 
 namespace itxq\phinx\facade;
 
+use Cake\Database\Query;
 use Phinx\Db\Adapter\AdapterInterface;
 use Phinx\Db\Table;
 use think\Facade;
 
 /**
  * Class Phinx
+ * @method int execute(string $sql, string $package = self::LOCAL, ?array $adapterOptions = null) static Execute查询
+ * @method mixed query(string $sql, string $package = self::LOCAL, ?array $adapterOptions = null) static Query查询
+ * @method array fetchRow(string $sql, string $package = self::LOCAL, ?array $adapterOptions = null) static fetchRow
+ * @method array fetchAll(string $sql, string $package = self::LOCAL, ?array $adapterOptions = null) static fetchAll
+ * @method Query getQueryBuilder(string $package = self::LOCAL, ?array $adapterOptions = null) static getQueryBuilder
+ * @method void createDatabase(string $name, $options, string $package = self::LOCAL, ?array $adapterOptions = null) static 创建数据库
+ * @method void dropDatabase(string $name, string $package = self::LOCAL, ?array $adapterOptions = null) static 删除数据库
+ * @method bool hasTable(string $tableName, string $package = self::LOCAL, ?array $adapterOptions = null) static 判断表是否存在
  * @method Table table(string $tableName, array $options = [], string $package = self::LOCAL, ?array $adapterOptions = null) static 获取Phinx\Db\Table实例
  * @method AdapterInterface getAdapter(string $package = self::LOCAL, ?array $options = null) static Get an adapter
  * @method array getPhinxConfig(string $package = self::LOCAL) static 获取配置
